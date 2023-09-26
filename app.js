447017395e666a3211795f2125443ed0888fac62
marvin <marvin010528@foxmail.com>
2023-09-26 16:52:47 +0800

    feat: 跨域

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require("express");
+const cors = require('cors');
 const logger = require("./middleware/logger");
 const userRouter = require("./router/user");
 const bookRouter = require("./router/book");
 const app = express();
 const port = 9000;
-
+app.use(cors())
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: false }));
 
