dc5ab543b0cff527fddfd13b9835f27bd29a57a9
marvin <marvin010528@foxmail.com>
2023-10-18 16:57:48 +0800

    logger

--- app.js
+++ app.js
@@ -9,12 +9,10 @@ app.use(cors())
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: false }));
 
+app.use(logger)
 app.use("/user", userRouter);
 app.use("/book", bookRouter);
-
-app.get("/", logger, (req, res) => {
-  return res.json({ name: "aa" });
-});
+// app.get("/**/*", logger);
 
 app.get("/chatflows/:id", (req, res) => {
   console.log(req.params.id);
